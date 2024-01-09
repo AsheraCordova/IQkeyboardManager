a962a2935588a4dfb8fa525187bea0cb0ab83a99
Ashera Cordova <ashera.cordova@gmail.com>
2024-01-09 23:16:00 +0530

    Added support for animateLayoutChanges.

--- ios/ios/src/IQPreviousNextViewImpl.m
+++ ios/ios/src/IQPreviousNextViewImpl.m
@@ -16,6 +16,7 @@
 #include "IWidget.h"
 #include "IWidgetLifeCycleListener.h"
 #include "J2ObjC_source.h"
+#include "LayoutTransition.h"
 #include "MeasureEvent.h"
 #include "OnLayoutEvent.h"
 #include "Rect.h"
@@ -27,6 +28,7 @@
 #include "WidgetFactory.h"
 #include "java/lang/Boolean.h"
 #include "java/lang/Integer.h"
+#include "java/lang/Runnable.h"
 #include "java/lang/UnsupportedOperationException.h"
 #include "java/util/HashMap.h"
 #include "java/util/List.h"
@@ -41,6 +43,7 @@
 @protocol JavaUtilMap;
 
 
+#pragma clang diagnostic ignored "-Wprotocol"
 #pragma clang diagnostic ignored "-Wincomplete-implementation"
 
 @interface ASIQPreviousNextViewImpl () {
@@ -54,6 +57,8 @@
 }
 
 - (void)setWidgetOnNativeClass;
+
+- (void)nativeRemoveViewWithASIWidget:(id<ASIWidget>)widget;
 
 - (void)createLayoutParamsWithADView:(ADView *)view;
 
@@ -69,6 +74,8 @@ J2OBJC_FIELD_SETTER(ASIQPreviousNextViewImpl, paramsBuilder_, ASIQPreviousNextVi
 J2OBJC_FIELD_SETTER(ASIQPreviousNextViewImpl, paramsBean_, ASIQPreviousNextViewImpl_IQPreviousNextViewParamsBean *)
 
 __attribute__((unused)) static void ASIQPreviousNextViewImpl_setWidgetOnNativeClass(ASIQPreviousNextViewImpl *self);
+
+__attribute__((unused)) static void ASIQPreviousNextViewImpl_nativeRemoveViewWithASIWidget_(ASIQPreviousNextViewImpl *self, id<ASIWidget> widget);
 
 __attribute__((unused)) static void ASIQPreviousNextViewImpl_createLayoutParamsWithADView_(ASIQPreviousNextViewImpl *self, ADView *view);
 
@@ -110,6 +117,23 @@ J2OBJC_FIELD_SETTER(ASIQPreviousNextViewImpl_IQPreviousNextViewExt, templates_, 
 }
 
 @end
+
+@interface ASIQPreviousNextViewImpl_$Lambda$1 : NSObject < JavaLangRunnable > {
+ @public
+  id<ASIWidget> val$widget_;
+}
+
+- (void)run;
+
+@end
+
+J2OBJC_EMPTY_STATIC_INIT(ASIQPreviousNextViewImpl_$Lambda$1)
+
+__attribute__((unused)) static void ASIQPreviousNextViewImpl_$Lambda$1_initWithASIWidget_(ASIQPreviousNextViewImpl_$Lambda$1 *self, id<ASIWidget> capture$0);
+
+__attribute__((unused)) static ASIQPreviousNextViewImpl_$Lambda$1 *new_ASIQPreviousNextViewImpl_$Lambda$1_initWithASIWidget_(id<ASIWidget> capture$0) NS_RETURNS_RETAINED;
+
+__attribute__((unused)) static ASIQPreviousNextViewImpl_$Lambda$1 *create_ASIQPreviousNextViewImpl_$Lambda$1_initWithASIWidget_(id<ASIWidget> capture$0);
 
 NSString *ASIQPreviousNextViewImpl_LOCAL_NAME = @"IQPreviousNextView";
 NSString *ASIQPreviousNextViewImpl_GROUP_NAME = @"IQPreviousNextView";
@@ -166,7 +190,7 @@ J2OBJC_IGNORE_DESIGNATED_END
 - (jboolean)removeWithASIWidget:(id<ASIWidget>)w {
   jboolean remove = [super removeWithASIWidget:w];
   [((ADFrameLayout *) nil_chk(frameLayout_)) removeViewWithADView:(ADView *) cast_chk([((id<ASIWidget>) nil_chk(w)) asWidget], [ADView class])];
-  ASViewGroupImpl_nativeRemoveViewWithASIWidget_(w);
+  ASIQPreviousNextViewImpl_nativeRemoveViewWithASIWidget_(self, w);
   return remove;
 }
 
@@ -175,9 +199,13 @@ J2OBJC_IGNORE_DESIGNATED_END
   jboolean remove = [super removeWithInt:index];
   if (index + 1 <= [((ADFrameLayout *) nil_chk(frameLayout_)) getChildCount]) {
     [((ADFrameLayout *) nil_chk(frameLayout_)) removeViewAtWithInt:index];
-    ASViewGroupImpl_nativeRemoveViewWithASIWidget_(widget);
+    ASIQPreviousNextViewImpl_nativeRemoveViewWithASIWidget_(self, widget);
   }
   return remove;
+}
+
+- (void)nativeRemoveViewWithASIWidget:(id<ASIWidget>)widget {
+  ASIQPreviousNextViewImpl_nativeRemoveViewWithASIWidget_(self, widget);
 }
 
 - (void)addWithASIWidget:(id<ASIWidget>)w
@@ -370,26 +398,27 @@ J2OBJC_IGNORE_DESIGNATED_END
     { NULL, "LNSObject;", 0x1, -1, -1, -1, -1, -1, -1 },
     { NULL, "Z", 0x1, 6, 7, -1, -1, -1, -1 },
     { NULL, "Z", 0x1, 6, 8, -1, -1, -1, -1 },
-    { NULL, "V", 0x1, 9, 10, -1, -1, -1, -1 },
-    { NULL, "V", 0x2, 11, 12, -1, -1, -1, -1 },
-    { NULL, "LADFrameLayout_LayoutParams;", 0x2, 13, 12, -1, -1, -1, -1 },
-    { NULL, "V", 0x1, 14, 15, -1, -1, -1, -1 },
-    { NULL, "LNSObject;", 0x1, 16, 17, -1, -1, -1, -1 },
+    { NULL, "V", 0x2, 9, 7, -1, -1, -1, -1 },
+    { NULL, "V", 0x1, 10, 11, -1, -1, -1, -1 },
+    { NULL, "V", 0x2, 12, 13, -1, -1, -1, -1 },
+    { NULL, "LADFrameLayout_LayoutParams;", 0x2, 14, 13, -1, -1, -1, -1 },
+    { NULL, "V", 0x1, 15, 16, -1, -1, -1, -1 },
+    { NULL, "LNSObject;", 0x1, 17, 18, -1, -1, -1, -1 },
     { NULL, "LIOSClass;", 0x1, -1, -1, -1, -1, -1, -1 },
-    { NULL, "V", 0x1, 18, 19, -1, -1, -1, -1 },
-    { NULL, "LNSObject;", 0x1, 20, 21, -1, -1, -1, -1 },
+    { NULL, "V", 0x1, 19, 20, -1, -1, -1, -1 },
+    { NULL, "LNSObject;", 0x1, 21, 22, -1, -1, -1, -1 },
     { NULL, "LNSObject;", 0x1, -1, -1, -1, -1, -1, -1 },
-    { NULL, "Z", 0x101, 22, 1, -1, -1, -1, -1 },
+    { NULL, "Z", 0x101, 23, 1, -1, -1, -1, -1 },
     { NULL, "V", 0x1, -1, -1, -1, -1, -1, -1 },
     { NULL, "V", 0x1, -1, -1, -1, -1, -1, -1 },
-    { NULL, "V", 0x1, 23, 1, -1, -1, -1, -1 },
-    { NULL, "V", 0x1, 24, 25, -1, -1, -1, -1 },
-    { NULL, "LNSObject;", 0x1, 26, 1, -1, -1, -1, -1 },
+    { NULL, "V", 0x1, 24, 1, -1, -1, -1, -1 },
+    { NULL, "V", 0x1, 25, 26, -1, -1, -1, -1 },
+    { NULL, "LNSObject;", 0x1, 27, 1, -1, -1, -1, -1 },
     { NULL, "LASIQPreviousNextViewImpl_IQPreviousNextViewBean;", 0x1, -1, -1, -1, -1, -1, -1 },
     { NULL, "LASIQPreviousNextViewImpl_IQPreviousNextViewCommandBuilder;", 0x1, -1, -1, -1, -1, -1, -1 },
     { NULL, "LASIQPreviousNextViewImpl_IQPreviousNextViewParamsBean;", 0x1, -1, -1, -1, -1, -1, -1 },
     { NULL, "LASIQPreviousNextViewImpl_IQPreviousNextViewCommandParamsBuilder;", 0x1, -1, -1, -1, -1, -1, -1 },
-    { NULL, "V", 0x101, 27, 28, -1, 29, -1, -1 },
+    { NULL, "V", 0x101, 28, 29, -1, 30, -1, -1 },
   };
   #pragma clang diagnostic push
   #pragma clang diagnostic ignored "-Wobjc-multiple-method-names"
@@ -404,39 +433,40 @@ J2OBJC_IGNORE_DESIGNATED_END
   methods[7].selector = @selector(asWidget);
   methods[8].selector = @selector(removeWithASIWidget:);
   methods[9].selector = @selector(removeWithInt:);
-  methods[10].selector = @selector(addWithASIWidget:withInt:);
-  methods[11].selector = @selector(createLayoutParamsWithADView:);
-  methods[12].selector = @selector(getLayoutParamsWithADView:);
-  methods[13].selector = @selector(setChildAttributeWithASIWidget:withASWidgetAttribute:withNSString:withId:);
-  methods[14].selector = @selector(getChildAttributeWithASIWidget:withASWidgetAttribute:);
-  methods[15].selector = @selector(getViewClass);
-  methods[16].selector = @selector(setAttributeWithASWidgetAttribute:withNSString:withId:withASILifeCycleDecorator:);
-  methods[17].selector = @selector(getAttributeWithASWidgetAttribute:withASILifeCycleDecorator:);
-  methods[18].selector = @selector(asNativeWidget);
-  methods[19].selector = @selector(checkIosVersionWithNSString:);
-  methods[20].selector = @selector(requestLayout);
-  methods[21].selector = @selector(invalidate);
-  methods[22].selector = @selector(setIdWithNSString:);
-  methods[23].selector = @selector(setVisibleWithBoolean:);
-  methods[24].selector = @selector(getPluginWithNSString:);
-  methods[25].selector = @selector(getBean);
-  methods[26].selector = @selector(getBuilder);
-  methods[27].selector = @selector(getParamsBean);
-  methods[28].selector = @selector(getParamsBuilder);
-  methods[29].selector = @selector(nativeCreateWithJavaUtilMap:);
+  methods[10].selector = @selector(nativeRemoveViewWithASIWidget:);
+  methods[11].selector = @selector(addWithASIWidget:withInt:);
+  methods[12].selector = @selector(createLayoutParamsWithADView:);
+  methods[13].selector = @selector(getLayoutParamsWithADView:);
+  methods[14].selector = @selector(setChildAttributeWithASIWidget:withASWidgetAttribute:withNSString:withId:);
+  methods[15].selector = @selector(getChildAttributeWithASIWidget:withASWidgetAttribute:);
+  methods[16].selector = @selector(getViewClass);
+  methods[17].selector = @selector(setAttributeWithASWidgetAttribute:withNSString:withId:withASILifeCycleDecorator:);
+  methods[18].selector = @selector(getAttributeWithASWidgetAttribute:withASILifeCycleDecorator:);
+  methods[19].selector = @selector(asNativeWidget);
+  methods[20].selector = @selector(checkIosVersionWithNSString:);
+  methods[21].selector = @selector(requestLayout);
+  methods[22].selector = @selector(invalidate);
+  methods[23].selector = @selector(setIdWithNSString:);
+  methods[24].selector = @selector(setVisibleWithBoolean:);
+  methods[25].selector = @selector(getPluginWithNSString:);
+  methods[26].selector = @selector(getBean);
+  methods[27].selector = @selector(getBuilder);
+  methods[28].selector = @selector(getParamsBean);
+  methods[29].selector = @selector(getParamsBuilder);
+  methods[30].selector = @selector(nativeCreateWithJavaUtilMap:);
   #pragma clang diagnostic pop
   static const J2ObjcFieldInfo fields[] = {
     { "uiView_", "LNSObject;", .constantValue.asLong = 0, 0x2, -1, -1, -1, -1 },
-    { "LOCAL_NAME", "LNSString;", .constantValue.asLong = 0, 0x19, -1, 30, -1, -1 },
-    { "GROUP_NAME", "LNSString;", .constantValue.asLong = 0, 0x19, -1, 31, -1, -1 },
+    { "LOCAL_NAME", "LNSString;", .constantValue.asLong = 0, 0x19, -1, 31, -1, -1 },
+    { "GROUP_NAME", "LNSString;", .constantValue.asLong = 0, 0x19, -1, 32, -1, -1 },
     { "frameLayout_", "LADFrameLayout;", .constantValue.asLong = 0, 0x2, -1, -1, -1, -1 },
     { "builder_", "LASIQPreviousNextViewImpl_IQPreviousNextViewCommandBuilder;", .constantValue.asLong = 0, 0x2, -1, -1, -1, -1 },
     { "bean_", "LASIQPreviousNextViewImpl_IQPreviousNextViewBean;", .constantValue.asLong = 0, 0x2, -1, -1, -1, -1 },
     { "paramsBuilder_", "LASIQPreviousNextViewImpl_IQPreviousNextViewCommandParamsBuilder;", .constantValue.asLong = 0, 0x2, -1, -1, -1, -1 },
     { "paramsBean_", "LASIQPreviousNextViewImpl_IQPreviousNextViewParamsBean;", .constantValue.asLong = 0, 0x2, -1, -1, -1, -1 },
   };
-  static const void *ptrTable[] = { "loadAttributes", "LNSString;", "LNSString;LNSString;", "create", "LASIFragment;LJavaUtilMap;", "(Lcom/ashera/core/IFragment;Ljava/util/Map<Ljava/lang/String;Ljava/lang/Object;>;)V", "remove", "LASIWidget;", "I", "add", "LASIWidget;I", "createLayoutParams", "LADView;", "getLayoutParams", "setChildAttribute", "LASIWidget;LASWidgetAttribute;LNSString;LNSObject;", "getChildAttribute", "LASIWidget;LASWidgetAttribute;", "setAttribute", "LASWidgetAttribute;LNSString;LNSObject;LASILifeCycleDecorator;", "getAttribute", "LASWidgetAttribute;LASILifeCycleDecorator;", "checkIosVersion", "setId", "setVisible", "Z", "getPlugin", "nativeCreate", "LJavaUtilMap;", "(Ljava/util/Map<Ljava/lang/String;Ljava/lang/Object;>;)V", &ASIQPreviousNextViewImpl_LOCAL_NAME, &ASIQPreviousNextViewImpl_GROUP_NAME, "LASIQPreviousNextViewImpl_IQPreviousNextViewExt;LASIQPreviousNextViewImpl_IQPreviousNextViewCommandBuilder;LASIQPreviousNextViewImpl_IQPreviousNextViewBean;LASIQPreviousNextViewImpl_IQPreviousNextViewParamsBean;LASIQPreviousNextViewImpl_IQPreviousNextViewCommandParamsBuilder;" };
-  static const J2ObjcClassInfo _ASIQPreviousNextViewImpl = { "IQPreviousNextViewImpl", "com.ashera.iqkeyboardmanager", ptrTable, methods, fields, 7, 0x1, 30, 8, -1, 32, -1, -1, -1 };
+  static const void *ptrTable[] = { "loadAttributes", "LNSString;", "LNSString;LNSString;", "create", "LASIFragment;LJavaUtilMap;", "(Lcom/ashera/core/IFragment;Ljava/util/Map<Ljava/lang/String;Ljava/lang/Object;>;)V", "remove", "LASIWidget;", "I", "nativeRemoveView", "add", "LASIWidget;I", "createLayoutParams", "LADView;", "getLayoutParams", "setChildAttribute", "LASIWidget;LASWidgetAttribute;LNSString;LNSObject;", "getChildAttribute", "LASIWidget;LASWidgetAttribute;", "setAttribute", "LASWidgetAttribute;LNSString;LNSObject;LASILifeCycleDecorator;", "getAttribute", "LASWidgetAttribute;LASILifeCycleDecorator;", "checkIosVersion", "setId", "setVisible", "Z", "getPlugin", "nativeCreate", "LJavaUtilMap;", "(Ljava/util/Map<Ljava/lang/String;Ljava/lang/Object;>;)V", &ASIQPreviousNextViewImpl_LOCAL_NAME, &ASIQPreviousNextViewImpl_GROUP_NAME, "LASIQPreviousNextViewImpl_IQPreviousNextViewExt;LASIQPreviousNextViewImpl_IQPreviousNextViewCommandBuilder;LASIQPreviousNextViewImpl_IQPreviousNextViewBean;LASIQPreviousNextViewImpl_IQPreviousNextViewParamsBean;LASIQPreviousNextViewImpl_IQPreviousNextViewCommandParamsBuilder;" };
+  static const J2ObjcClassInfo _ASIQPreviousNextViewImpl = { "IQPreviousNextViewImpl", "com.ashera.iqkeyboardmanager", ptrTable, methods, fields, 7, 0x1, 31, 8, -1, 33, -1, -1, -1 };
   return &_ASIQPreviousNextViewImpl;
 }
 
@@ -480,6 +510,16 @@ ASIQPreviousNextViewImpl *create_ASIQPreviousNextViewImpl_initWithNSString_withN
 
 void ASIQPreviousNextViewImpl_setWidgetOnNativeClass(ASIQPreviousNextViewImpl *self) {
   ((ASUIView*) [self asNativeWidget]).widget = self;
+}
+
+void ASIQPreviousNextViewImpl_nativeRemoveViewWithASIWidget_(ASIQPreviousNextViewImpl *self, id<ASIWidget> widget) {
+  ADLayoutTransition *layoutTransition = [((ADFrameLayout *) nil_chk(self->frameLayout_)) getLayoutTransition];
+  if (layoutTransition != nil && ([layoutTransition isTransitionTypeEnabledWithInt:ADLayoutTransition_CHANGE_DISAPPEARING] || [layoutTransition isTransitionTypeEnabledWithInt:ADLayoutTransition_DISAPPEARING])) {
+    [self addToBufferedRunnablesWithJavaLangRunnable:new_ASIQPreviousNextViewImpl_$Lambda$1_initWithASIWidget_(widget)];
+  }
+  else {
+    ASViewGroupImpl_nativeRemoveViewWithASIWidget_(widget);
+  }
 }
 
 void ASIQPreviousNextViewImpl_createLayoutParamsWithADView_(ASIQPreviousNextViewImpl *self, ADView *view) {
@@ -726,6 +766,11 @@ J2OBJC_CLASS_TYPE_LITERAL_SOURCE(ASIQPreviousNextViewImpl)
   ASViewImpl_stateNoWithASIWidget_(this$0_);
 }
 
+- (void)endViewTransitionWithADView:(ADView *)view {
+  [super endViewTransitionWithADView:view];
+  [this$0_ runBufferedRunnables];
+}
+
 - (void)__javaClone:(ASIQPreviousNextViewImpl_IQPreviousNextViewExt *)original {
   [super __javaClone:original];
   JreRelease(this$0_);
@@ -770,6 +815,7 @@ J2OBJC_CLASS_TYPE_LITERAL_SOURCE(ASIQPreviousNextViewImpl)
     { NULL, "V", 0x1, -1, -1, -1, -1, -1, -1 },
     { NULL, "V", 0x1, -1, -1, -1, -1, -1, -1 },
     { NULL, "V", 0x1, -1, -1, -1, -1, -1, -1 },
+    { NULL, "V", 0x1, 35, 36, -1, -1, -1, -1 },
   };
   #pragma clang diagnostic push
   #pragma clang diagnostic ignored "-Wobjc-multiple-method-names"
@@ -811,6 +857,7 @@ J2OBJC_CLASS_TYPE_LITERAL_SOURCE(ASIQPreviousNextViewImpl)
   methods[34].selector = @selector(state4);
   methods[35].selector = @selector(stateYes);
   methods[36].selector = @selector(stateNo);
+  methods[37].selector = @selector(endViewTransitionWithADView:);
   #pragma clang diagnostic pop
   static const J2ObjcFieldInfo fields[] = {
     { "this$0_", "LASIQPreviousNextViewImpl;", .constantValue.asLong = 0, 0x1012, -1, -1, -1, -1 },
@@ -818,10 +865,10 @@ J2OBJC_CLASS_TYPE_LITERAL_SOURCE(ASIQPreviousNextViewImpl)
     { "onLayoutEvent_", "LASOnLayoutEvent;", .constantValue.asLong = 0, 0x2, -1, -1, -1, -1 },
     { "mMaxWidth_", "I", .constantValue.asLong = 0, 0x2, -1, -1, -1, -1 },
     { "mMaxHeight_", "I", .constantValue.asLong = 0, 0x2, -1, -1, -1, -1 },
-    { "templates_", "LJavaUtilMap;", .constantValue.asLong = 0, 0x2, -1, -1, 35, -1 },
+    { "templates_", "LJavaUtilMap;", .constantValue.asLong = 0, 0x2, -1, -1, 37, -1 },
   };
-  static const void *ptrTable[] = { "setMaxWidth", "I", "setMaxHeight", "LASIQPreviousNextViewImpl;", "onMeasure", "II", "onLayout", "ZIIII", "execute", "LNSString;[LNSObject;", "updateMeasuredDimension", "newInstance", "LASIWidget;", "setAttribute", "LASWidgetAttribute;LNSString;LNSObject;", "()Ljava/util/List<Ljava/lang/String;>;", "getAttribute", "LASWidgetAttribute;", "inflateView", "LNSString;", "getLocationOnScreen", "[I", "getWindowVisibleDisplayFrame", "LADRect;", "offsetTopAndBottom", "offsetLeftAndRight", "setMyAttribute", "LNSString;LNSObject;", "setVisibility", "setState0", "LNSObject;", "setState1", "setState2", "setState3", "setState4", "Ljava/util/Map<Ljava/lang/String;Lcom/ashera/widget/IWidget;>;" };
-  static const J2ObjcClassInfo _ASIQPreviousNextViewImpl_IQPreviousNextViewExt = { "IQPreviousNextViewExt", "com.ashera.iqkeyboardmanager", ptrTable, methods, fields, 7, 0x1, 37, 6, 3, -1, -1, -1, -1 };
+  static const void *ptrTable[] = { "setMaxWidth", "I", "setMaxHeight", "LASIQPreviousNextViewImpl;", "onMeasure", "II", "onLayout", "ZIIII", "execute", "LNSString;[LNSObject;", "updateMeasuredDimension", "newInstance", "LASIWidget;", "setAttribute", "LASWidgetAttribute;LNSString;LNSObject;", "()Ljava/util/List<Ljava/lang/String;>;", "getAttribute", "LASWidgetAttribute;", "inflateView", "LNSString;", "getLocationOnScreen", "[I", "getWindowVisibleDisplayFrame", "LADRect;", "offsetTopAndBottom", "offsetLeftAndRight", "setMyAttribute", "LNSString;LNSObject;", "setVisibility", "setState0", "LNSObject;", "setState1", "setState2", "setState3", "setState4", "endViewTransition", "LADView;", "Ljava/util/Map<Ljava/lang/String;Lcom/ashera/widget/IWidget;>;" };
+  static const J2ObjcClassInfo _ASIQPreviousNextViewImpl_IQPreviousNextViewExt = { "IQPreviousNextViewExt", "com.ashera.iqkeyboardmanager", ptrTable, methods, fields, 7, 0x1, 38, 6, 3, -1, -1, -1, -1 };
   return &_ASIQPreviousNextViewImpl_IQPreviousNextViewExt;
 }
 
@@ -1104,3 +1151,24 @@ ASIQPreviousNextViewImpl_IQPreviousNextViewCommandParamsBuilder *create_ASIQPrev
 }
 
 J2OBJC_CLASS_TYPE_LITERAL_SOURCE(ASIQPreviousNextViewImpl_IQPreviousNextViewCommandParamsBuilder)
+
+@implementation ASIQPreviousNextViewImpl_$Lambda$1
+
+- (void)run {
+  ASViewGroupImpl_nativeRemoveViewWithASIWidget_(val$widget_);
+}
+
+@end
+
+void ASIQPreviousNextViewImpl_$Lambda$1_initWithASIWidget_(ASIQPreviousNextViewImpl_$Lambda$1 *self, id<ASIWidget> capture$0) {
+  self->val$widget_ = capture$0;
+  NSObject_init(self);
+}
+
+ASIQPreviousNextViewImpl_$Lambda$1 *new_ASIQPreviousNextViewImpl_$Lambda$1_initWithASIWidget_(id<ASIWidget> capture$0) {
+  J2OBJC_NEW_IMPL(ASIQPreviousNextViewImpl_$Lambda$1, initWithASIWidget_, capture$0)
+}
+
+ASIQPreviousNextViewImpl_$Lambda$1 *create_ASIQPreviousNextViewImpl_$Lambda$1_initWithASIWidget_(id<ASIWidget> capture$0) {
+  J2OBJC_CREATE_IMPL(ASIQPreviousNextViewImpl_$Lambda$1, initWithASIWidget_, capture$0)
+}
