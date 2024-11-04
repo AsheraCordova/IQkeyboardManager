f1bd255b2dac2c67776ab59ebe2939d411fc99fc
Ashera Cordova <ashera.cordova@gmail.com>
2024-11-04 23:13:27 +0530

    Regression test bug fixes

--- ios/src/main/java/com/ashera/iqkeyboardmanager/IQkeyboardManagerImpl.java
+++ ios/src/main/java/com/ashera/iqkeyboardmanager/IQkeyboardManagerImpl.java
@@ -158,7 +158,9 @@ public boolean onEditorAction (TextView v,
 		}
 		if (strValue != null && !strValue.isEmpty() && !strValue.trim().startsWith("+")) {
 		    com.ashera.core.IActivity activity = (com.ashera.core.IActivity)w.getFragment().getRootActivity();
-		    activity.sendEventMessage(obj);
+		    if (activity != null) {
+		    	activity.sendEventMessage(obj);
+		    }
 		}
 	}
     return result;
